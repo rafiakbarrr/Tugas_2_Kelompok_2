445adcf0f3db5f9e02b897cf0d9fb6a2e942a314
rafiakbar <admin@rafiakbar.com>
2023-10-06 22:45:49 +0700

    nambah validasi

--- Apotek/Models/Obat.cs
+++ Apotek/Models/Obat.cs
@@ -1,11 +1,23 @@
-﻿namespace Apotek.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Apotek.Models
 {
     public class Obat
     {
+        [Required(ErrorMessage = "ID Obat wajib diisi.")]
+        [RegularExpression("^OBA\\d{3}$", ErrorMessage = "Format ID tidak valid. Gunakan format OBAXXX.")]
         public string idObat { get; set; }
+
+        [Required(ErrorMessage = "ID Jenis wajib diisi.")]
         public int idJenis { get; set; }
+
+        [Required(ErrorMessage = "Nama Obat wajib diisi.")]
+        [MaxLength(30, ErrorMessage = "Nama Obat maksimal 30 karakter.")]
         public string namaObat { get; set;}
         public DateTime tanggalKadaluarsaObat { get; set;}
+
+        [Required(ErrorMessage = "harga wajib diisi.")]
+        [RegularExpression("^[0-9]$", ErrorMessage = "Inputan hanya bisa Angka")]
         public double hargaObat { get; set; }
     }
 }
--- Apotek/Models/Pembelian.cs
+++ Apotek/Models/Pembelian.cs
@@ -1,10 +1,20 @@
-﻿namespace Apotek.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Apotek.Models
 {
     public class Pembelian
     {
+        [Required(ErrorMessage = "ID Transaksi wajib diisi.")]
+        [RegularExpression("^TRS\\d{3}$", ErrorMessage = "Format ID tidak valid. Gunakan format TRSXXX.")]
         public string idTransaksi { get; set; }
         public DateTime tanggalTransaksi { get; set; }
+
+        [Required(ErrorMessage = "ID Obat wajib diisi.")]
+        [RegularExpression("^OBA\\d{3}$", ErrorMessage = "Format ID tidak valid. Gunakan format OBAXXX.")]
         public string idObat { get; set; }
+
+        [Required(ErrorMessage = "jumlah wajib diisi.")]
+        [RegularExpression("^[0-9]$", ErrorMessage = "Inputan hanya bisa Angka")]
         public int jumlah { get; set; }
         public double totalHarga { get; set; }
     }
